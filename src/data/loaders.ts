cb089c9ca1912ea06fb9f110f510d57674612e5f
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-11-07 17:13:08 +0500

    feat: add loaders.ts to centralize fetching logic

---
+++ src/data/loaders.ts
@@ -0,0 +1,39 @@
+import qs from 'qs';
+import { getStrapiURL } from '@/lib/utils';
+
+const baseUrl = getStrapiURL();
+
+async function fetchData(url: string) {
+  const authToken = null; // we will implement this later getAuthToken() later
+  const headers = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authToken}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, authToken ? headers : {});
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error; // or return null;
+  }
+}
+
+export async function getNavigationData() {
+  const url = new URL('/api/navigation', baseUrl);
+
+  url.search = qs.stringify({
+    populate: {
+      navItem: true,
+      image: {
+        fields: ['url', 'alternativeText'],
+      },
+    },
+  });
+
+  return await fetchData(url.href);
+}
