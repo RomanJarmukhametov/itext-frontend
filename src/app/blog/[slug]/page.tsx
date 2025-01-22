55c20df2b302b4674e3e72e2fe55688c7c1fb8ef
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2025-01-22 09:42:08 +0500

    feat: remove animated section from blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -80,47 +80,45 @@ export default async function SinglePost({ params }: Props) {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
 
-      <AnimatedSection>
-        <section className="py-16 md:py-24 bg-whitebg-pattern-white">
-          <div className="container px-4 mx-auto">
-            <div className="md:max-w-2xl mx-auto mb-12 text-center">
-              <div className="flex items-center justify-center">
-                <p className="inline-block text-blue-500 font-medium">{post.author}</p>
-                <span className="mx-1 text-blue-500">•</span>
-                <p className="inline-block text-blue-500 font-medium">
-                  {moment(post.publishedAt).format('D MMMM YYYY')}
-                </p>
-              </div>
-              <Heading level={1}>{post.title}</Heading>
-              <BodyText variant="large" text={post.subtitle} className="mb-6" />
-
-              <div className="inline-block py-1 px-3 text-xs leading-5 text-blue-500 font-medium uppercase bg-blue-100 rounded-full shadow-sm">
-                {post.category}
-              </div>
-            </div>
-
-            <div className="mb-10 mx-auto max-w-5xl overflow-hidden rounded-lg">
-              <StrapiImage
-                src={post.image.url}
-                alt={post.image.alternativeText}
-                width={1200}
-                height={800}
-                priority
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-
-            <div className="md:max-w-3xl mx-auto">
-              <BlockRendererClient content={post.content} />
-            </div>
-
+      <section className="py-16 md:py-24 bg-whitebg-pattern-white">
+        <div className="container px-4 mx-auto">
+          <div className="md:max-w-2xl mx-auto mb-12 text-center">
             <div className="flex items-center justify-center">
-              <CopyLinkButton />
-              <SocialMediaShareButtons postUrl={postUrl} postTitle={postTitle} />
+              <p className="inline-block text-blue-500 font-medium">{post.author}</p>
+              <span className="mx-1 text-blue-500">•</span>
+              <p className="inline-block text-blue-500 font-medium">
+                {moment(post.publishedAt).format('D MMMM YYYY')}
+              </p>
+            </div>
+            <Heading level={1}>{post.title}</Heading>
+            <BodyText variant="large" text={post.subtitle} className="mb-6" />
+
+            <div className="inline-block py-1 px-3 text-xs leading-5 text-blue-500 font-medium uppercase bg-blue-100 rounded-full shadow-sm">
+              {post.category}
             </div>
           </div>
-        </section>
-      </AnimatedSection>
+
+          <div className="mb-10 mx-auto max-w-5xl overflow-hidden rounded-lg">
+            <StrapiImage
+              src={post.image.url}
+              alt={post.image.alternativeText}
+              width={1200}
+              height={800}
+              priority
+              className="w-full h-auto rounded-lg"
+            />
+          </div>
+
+          <div className="md:max-w-3xl mx-auto">
+            <BlockRendererClient content={post.content} />
+          </div>
+
+          <div className="flex items-center justify-center">
+            <CopyLinkButton />
+            <SocialMediaShareButtons postUrl={postUrl} postTitle={postTitle} />
+          </div>
+        </div>
+      </section>
       <AnimatedSection>
         <Subscribe />
       </AnimatedSection>
